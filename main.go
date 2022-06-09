2e3116ea55a82716d2af717e1a4ffc7646f05202
Roni Purwanto <ahmadroni@gmail.com>
2022-06-09 21:07:11 +0700

    add logic 4

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/aronipurwanto/golang-logic-dasar/logic03"
+import (
+	"fmt"
+	"github.com/aronipurwanto/golang-logic-dasar/logic03"
+	"github.com/aronipurwanto/golang-logic-dasar/logic04"
+)
 
 func main() {
 	logic03.Logic03Soal03(9)
+	fmt.Println()
+	fmt.Println("Logic 4 - Soal 1")
+	logic04.Logic04Soal01(9)
+
+	fmt.Println()
+	fmt.Println("Logic 4 - Soal 2")
+	logic04.Logic04Soal02(9)
 }
