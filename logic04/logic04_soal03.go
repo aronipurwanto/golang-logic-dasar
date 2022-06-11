8bcb57dd690b1adc9ee3831f5778b6c3a068d2bf
Roni Purwanto <ahmadroni@gmail.com>
2022-06-11 14:12:52 +0700

    feat: add logic04 soal02, soal 03, soal 04

--- logic04/logic04_soal03.go
+++ logic04/logic04_soal03.go
@@ -4,7 +4,7 @@ import (
 	array2 "github.com/aronipurwanto/golang-logic-dasar/array"
 )
 
-func Logic04Soal02(n int) {
+func Logic04Soal03(n int) {
 	// create array
 	array := array2.NewNumberArray(n, n)
 	// initial angka dari 1
@@ -15,15 +15,15 @@ func Logic04Soal02(n int) {
 		for k := 0; k < n; k++ {
 			// jika baris 0
 			if b%4 == 0 {
-				array[k][b] = int32(angka)
+				array[n-1-k][b] = int32(angka)
 				angka += 3
-			} else if b%4 == 1 && k == n-1 {
+			} else if b%4 == 1 && k == 0 {
 				array[k][b] = int32(angka)
 				angka += 3
 			} else if b%4 == 2 {
-				array[n-1-k][b] = int32(angka)
+				array[k][b] = int32(angka)
 				angka += 3
-			} else if b%4 == 3 && k == 0 {
+			} else if b%4 == 3 && k == n-1 {
 				array[k][b] = int32(angka)
 				angka += 3
 			}
--- logic04/logic04_soal04.go
+++ logic04/logic04_soal04.go
@@ -4,28 +4,28 @@ import (
 	array2 "github.com/aronipurwanto/golang-logic-dasar/array"
 )
 
-func Logic04Soal02(n int) {
+func Logic04Soal04(n int) {
 	// create array
 	array := array2.NewNumberArray(n, n)
 	// initial angka dari 1
 	angka := 1
 	// loop baris
 	for b := 0; b < n; b++ {
-		// loop kolom
+		// loop kolomg
 		for k := 0; k < n; k++ {
 			// jika baris 0
 			if b%4 == 0 {
-				array[k][b] = int32(angka)
-				angka += 3
-			} else if b%4 == 1 && k == n-1 {
-				array[k][b] = int32(angka)
-				angka += 3
+				array[b][n-1-k] = int32(angka)
+				angka++
+			} else if b%4 == 1 && k == 0 {
+				array[b][k] = int32(angka)
+				angka++
 			} else if b%4 == 2 {
-				array[n-1-k][b] = int32(angka)
-				angka += 3
-			} else if b%4 == 3 && k == 0 {
-				array[k][b] = int32(angka)
-				angka += 3
+				array[b][k] = int32(angka)
+				angka++
+			} else if b%4 == 3 && k == n-1 {
+				array[b][k] = int32(angka)
+				angka++
 			}
 		}
 	}
